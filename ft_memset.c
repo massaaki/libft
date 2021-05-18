22bdd6415444c0b7d223a001140b739ec543fb0c
Massaaki <mauricio.massaaki@gmail.com>
2021-05-17 21:29:03 -0300

    memset: addapt to norminette

--- ft_memset.c
+++ ft_memset.c
@@ -6,28 +6,18 @@
 /*   By: mmassaak <mmassaak@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/17 19:28:41 by mmassaak          #+#    #+#             */
-/*   Updated: 2021/05/17 20:37:52 by mmassaak         ###   ########.fr       */
+/*   Updated: 2021/05/17 21:28:39 by mmassaak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*
- * Function MEMSET
- * Params:
- * 	- b:    destination pointer
- * 	- c:    unsigned char to set 'b'
- * 	- len:  length to set in b
- */
-
-
 #include "libft.h"
 
-void * ft_memset(void *b, int c, size_t len)
+void	*ft_memset(void *b, int c, size_t len)
 {
-	size_t i;
-	
-	if (!b)
-		return NULL;
+	size_t	i;
 
+	if (!b)
+		return (NULL);
 	i = 0;
 	while (i < len)
 	{
