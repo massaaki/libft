646017a42f1a569c75178c05bb5b73e956078bfb
Massaaki <mauricio.massaaki@gmail.com>
2021-05-18 00:01:14 -0300

    function: add ft_memcpy

--- Makefile
+++ Makefile
@@ -6,11 +6,11 @@
 #    By: mmassaak <mmassaak@student.42sp.org.br>    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/05/16 22:39:04 by mmassaak          #+#    #+#              #
-#    Updated: 2021/05/17 21:52:40 by mmassaak         ###   ########.fr        #
+#    Updated: 2021/05/17 23:41:59 by mmassaak         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-SRC = ft_strlen.c ft_bzero.c ft_memset.c
+SRC = ft_strlen.c ft_bzero.c ft_memset.c ft_memcpy.c
 OUT_FILES = $(SRC:.c=.o)# substitute .c to .o to all SRC
 NAME = libft.a
 FLAGS = -Wall -Werror -Wextra
--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,23 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mmassaak <mmassaak@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/05/16 22:26:20 by mmassaak          #+#    #+#             */
-/*   Updated: 2021/05/17 23:42:38 by mmassaak         ###   ########.fr       */
+/*   Created: 2021/05/17 23:23:27 by mmassaak          #+#    #+#             */
+/*   Updated: 2021/05/17 23:53:51 by mmassaak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-# include<stdlib.h>
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	size_t	i;
 
-size_t	ft_strlen(const char *s);
-void	*ft_memset(void *b, int c, size_t len);
-void	ft_bzero(void *s, size_t n);
-void	*ft_memcpy(void *dst, const void *src, size_t n);
-
-#endif
+	if (dst == src)
+		return (dst);
+	i = 0;
+	while (i < n)
+	{
+		*(char *)(dst + i) = *(char *)(src + i);
+		i++;
+	}
+	return (dst);
+}
