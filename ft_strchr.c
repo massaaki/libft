c3d6bba2bff7da6021eab99897e1647d703018ef
Massaaki <mauricio.massaaki@gmail.com>
2021-05-21 23:12:21 -0300

    refactor: Adjust ft_strchr && ft_strrchr to norminette sintax

--- ft_strchr.c
+++ ft_strchr.c
@@ -2,21 +2,18 @@
 
 char	*ft_strchr(const char *s, int c)
 {
-	int i;
-	char *ptr_s;
+	int			i;
+	char		*ptr_s;
 
 	ptr_s = (char *)s;
 	i = 0;
-	
-	
-	while(*(ptr_s + i))
+	while (*(ptr_s + i))
 	{
-		if(*(ptr_s + i) == (char)c)
+		if (*(ptr_s + i) == (char)c)
 			return (ptr_s + i);
 		i++;
 	}
 	if (c == '\0')
 		return (ptr_s + i);
-	
 	return (NULL);
 }
--- ft_strrchr.c
+++ ft_strrchr.c
@@ -2,8 +2,8 @@
 
 char	*ft_strrchr(const char *s, int c)
 {
-	int i;
-	char *ptr_s;
+	int		i;
+	char	*ptr_s;
 
 	ptr_s = (char *)s;
 	i = ft_strlen(s);
@@ -13,9 +13,7 @@ char	*ft_strrchr(const char *s, int c)
 			return (ptr_s + i);
 		i--;
 	}
-
-	if(c == '\0')
+	if (c == '\0')
 		return (ptr_s + ft_strlen(s) + 1);
-
 	return (NULL);
 }
