77c934f02931ea13ba1323890e798457854fa1bf
Massaaki <mauricio.massaaki@gmail.com>
2021-05-22 17:35:42 -0300

    refactor: adjust ft_isalnum, ft_isalpha and ft_isdigit to norminette sintax

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -1,8 +1,9 @@
 #include "libft.h"
-#include <stdio.h>
-int ft_isalnum(int c)
+
+int	ft_isalnum(int c)
 {
-	if(((unsigned char)c >= 'a' && (unsigned char)c <= 'z') || ((unsigned char)c >= 'A' && (unsigned char)c <= 'Z')
+	if (((unsigned char)c >= 'a' && (unsigned char)c <= 'z')
+		|| ((unsigned char)c >= 'A' && (unsigned char)c <= 'Z')
 		|| ((unsigned char)c >= '0' && (unsigned char)c <= '9'))
 		return (1);
 	return (0);
--- ft_isalpha.c
+++ ft_isalpha.c
@@ -1,8 +1,9 @@
 #include "libft.h"
 
-int ft_isalpha(int c)
+int	ft_isalpha(int c)
 {
-	if(((unsigned char)c >= 'a' && (unsigned char)c <= 'z') || ((unsigned char)c >= 'A' && (unsigned char)c <= 'Z'))
+	if (((unsigned char)c >= 'a' && (unsigned char)c <= 'z')
+		|| ((unsigned char)c >= 'A' && (unsigned char)c <= 'Z'))
 		return (1);
 	return (0);
 }
