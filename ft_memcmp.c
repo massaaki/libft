5c4e16a54c0667d31bc392ba15fffd57dc8feeba
Massaaki <mauricio.massaaki@gmail.com>
2021-05-23 07:40:53 -0300

    fix: remove end of string comparation in ft_memcmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -2,11 +2,14 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	int	i;
+	size_t	i;
 
 	i = 0;
-	while (i < ((int)n - 1)
-		&& *(unsigned char *)(s1 + i) == *(unsigned char *)(s2 + i))
+	while (i < n)
+	{
+		if(*(unsigned char *)(s1 + i) != *(unsigned char *)(s2 + i))
+			return (*(unsigned char *)(s1 + i) - *(unsigned char *)(s2 + i));
 		i++;
-	return (*(unsigned char *)(s1 + i) - *(unsigned char *)(s2 + i));
+	}
+	return (0);
 }
