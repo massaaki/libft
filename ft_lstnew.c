ee8193a370a64d5f1b7792dd2a22d297688acac7
Massaaki <mauricio.massaaki@gmail.com>
2021-05-30 21:10:40 -0300

    fix: add NULL to next to ensure current point to final

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -5,6 +5,9 @@ t_list	*ft_lstnew(void *content)
 	t_list	*list;
 
 	list = (t_list *)malloc(sizeof(t_list));
+	if (!list)
+		return (NULL);
 	list->content = content;
+	list->next = NULL;
 	return (list);
 }
