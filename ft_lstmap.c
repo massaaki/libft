0917ac1b823c67525b90c5db881bc2f654ab362d
Massaaki <mauricio.massaaki@gmail.com>
2021-06-10 23:49:39 -0300

    refactor(ft_lstmap): remove unnecessary first parameter and add NULL instead

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -4,23 +4,19 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new_el;
 	t_list	*first_new_el;
+	void	*temp;
 
 	if (!lst || !f)
 		return (NULL);
-	new_el = ft_lstnew(f(lst->content));
-	if (!new_el)
-	{
-		ft_lstclear(&lst, del);
-		return (NULL);
-	}
-	first_new_el = new_el;
-	lst = lst->next;
+	first_new_el = NULL;
 	while (lst != NULL)
 	{
-		new_el = ft_lstnew(f(lst->content));
+		temp = f(lst->content);
+		new_el = ft_lstnew(temp);
 		if (!new_el)
 		{
 			ft_lstclear(&first_new_el, del);
+			del(temp);
 			return (NULL);
 		}
 		lst = lst->next;
