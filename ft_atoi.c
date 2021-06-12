542d16a6df00bbaca7ed79e55d11a5c19a191118
Massaaki <mauricio.massaaki@gmail.com>
2021-06-12 20:21:52 -0300

    refactor(ft_atoi): remove unnecessary param

--- ft_atoi.c
+++ ft_atoi.c
@@ -1,10 +1,9 @@
 #include "libft.h"
 
-static void	ft_atoi_initialize(int *i, int *signal, int *has_signal)
+static void	ft_atoi_initialize(int *i, int *signal)
 {
 	*i = 0;
 	*signal = 1;
-	*has_signal = 0;
 }
 
 static void	ft_process_spaces(const char *str, int *i)
@@ -15,28 +14,23 @@ static void	ft_process_spaces(const char *str, int *i)
 }
 
 static void	ft_process_signal(const char *str, int *i,
-		int *signal, int *has_signal)
+		int *signal)
 {	
 	if (*(str + *i) == '-')
 	{
-		*has_signal = 1;
 		*signal = -1;
 		(*i)++;
 	}
 	else if (*(str + *i) == '+')
-	{
-		*has_signal = 1;
 		(*i)++;
-	}
 }
 
-static int	ft_process_result(const char *str, int i,
-		int signal, int has_signal)
+static int	ft_process_result(const char *str, int i, int signal)
 {
 	int	result;
 
 	result = 0;
-	if ((has_signal) && (*(str + i) == '-' || *(str + i) == '+'))
+	if (*(str + i) == '-' || *(str + i) == '+')
 		return (0);
 	while (*(str + i) >= '0' && *(str + i) <= '9')
 	{
@@ -50,10 +44,9 @@ int	ft_atoi(const char *str)
 {
 	int	i;
 	int	signal;
-	int	has_signal;
 
-	ft_atoi_initialize(&i, &signal, &has_signal);
+	ft_atoi_initialize(&i, &signal);
 	ft_process_spaces(str, &i);
-	ft_process_signal(str, &i, &signal, &has_signal);
-	return (ft_process_result(str, i, signal, has_signal));
+	ft_process_signal(str, &i, &signal);
+	return (ft_process_result(str, i, signal));
 }
